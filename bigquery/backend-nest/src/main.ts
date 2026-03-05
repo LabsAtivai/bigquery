955681f2c9d06ac45a25e2d3f76ab9da4bcb3c00
LabsAtivai <150172053+LabsAtivai@users.noreply.github.com>
2026-03-05 15:26:30 -0300

    Update main.ts

--- bigquery/backend-nest/src/main.ts
+++ bigquery/backend-nest/src/main.ts
@@ -4,13 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');  // ← adiciona /api/ em todas rotas
+
   app.enableCors({
-  origin: [
-    'https://bigquery.labsativa.com.br',
-    'http://localhost:5173'
-  ],
-  credentials: true,
-});
+    origin: ['https://bigquery.labsativa.com.br', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   await app.listen(3000);
 }
